b47d5bda54edfef22d7a8ae3d3e7356e97344cd3
liamsc8 <104771417+liamsc8@users.noreply.github.com>
2022-10-15 16:08:18 -0500

    Update autowinsetup.bat

--- autowinsetup.bat
+++ autowinsetup.bat
@@ -1,11 +1,10 @@
 ::setting up the cmd
 @echo off
 title Automatic Windows Setup
-color 9
 
 echo -----------------------------------------------------------------------------------------
 echo Automatic Windows Setup
-echo V1.0.2
+echo V1.0.2 Open Beta
 echo ----------------------WARNING----------------------
 echo Please make sure you run this as admin and in audit mode! This will only work on NEW windows installations.
 echo ---------------------------------------------------
@@ -15,12 +14,12 @@ NET LOCALGROUP Administrators %UserName% /ADD
 echo Created Account %UserName%
 echo -----------------------------------------------------------------------------------------
 echo Adding necessary registry edits........
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OOBE" /v "DisablePrivacyExperience" /t REG_DWORD /d 1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Polices\System" /v "EnableCursorSupprression" /t REG_DWORD /d 0
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /v "OOBEInProgress" /t REG_DWORD /d 0
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /v "SetupPhase" /t REG_DWORD /d 0
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /v "SetupType" /t REG_DWORD /d 0
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "VerboseStatus" /t REG_DWORD /d 1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OOBE" /f /v "DisablePrivacyExperience" /t REG_DWORD /d 1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Polices\System" /f /v "EnableCursorSupprression" /t REG_DWORD /d 0
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /f /v "OOBEInProgress" /t REG_DWORD /d 0
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /f /v "SetupPhase" /t REG_DWORD /d 0
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\Setup" /f /v "SetupType" /t REG_DWORD /d 0
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "VerboseStatus" /t REG_DWORD /d 1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\SYSTEM\Setup" /f /v "CmdLine" /t REG_SZ /d " "
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\SYSTEM\Setup" /f /v "RespecializeCmdLine" /t REG_SZ /d " "
 cls
